cdc4cc0c22cd7fa2d4f9c5e61d66a8213e98e6c4
Hello0801World <shun19036@yahoo.co.jp>
2023-05-28 03:50:19 -0500

    prevState example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import Setup from './tutorial/1-useState/setup/4-useState-object'
-import Final from './tutorial/1-useState/final/1-error-example'
+import Setup from './tutorial/1-useState/setup/5-useState-counter'
+import Final from './tutorial/1-useState/final/3-useState-array'
 
 function App() {
   return (
     <div className='container'>
-      <Setup />
+      {/* <Setup /> */}
+      <Final />
     </div>
   );
 }
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,50 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
-};
+  const [counter, setCounter ] = useState(0)
+
+  const decrease = () =>{
+    setCounter(counter-1)
+  }
+
+  const increase = () => {
+    setCounter(counter+1)
+  }
+
+  const reset = () => {
+    setCounter(0)
+  }
+
+  const complexIncrease = () => {
+    setTimeout(()=>{
+      // setCounter(counter + 1)
+      setCounter((prevState)=>{
+         console.log(prevState)
+        return prevState + 1;
+       })
+    }, 2000) // run 2000 ms (2 seconds) later
+  };
+
+   return (
+    <>
+      <section style={{margin:'4rem 8'}}>
+        <h2>
+            regular counter
+        </h2>
+        <h1>{counter}</h1>
+        <button className="btn" onClick={decrease}>Decrease</button>
+        <button className="btn" onClick={increase}>increase</button>
+        <button className="btn" onClick={reset}>reset</button>
+      </section>
+
+      <section style={{margin:'4rem 8'}}>
+        <h2>
+            More Complex counter
+        </h2>
+        <h1>{counter}</h1>
+        <button className="btn" onClick={complexIncrease}>increase later</button>
+      </section>
+    </>
+  )};
 
 export default UseStateCounter;
