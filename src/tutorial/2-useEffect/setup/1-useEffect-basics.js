2a361c514b1803c712b5a45eb64e12055d3c3e9f
Hello0801World <shun19036@yahoo.co.jp>
2023-05-29 17:53:43 -0500

    useEffect example

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,31 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue ] = useState(0)
+
+  // useEffect runs after every rendering
+  
+  useEffect(() => {
+    console.log('call useEffect');
+    if (value > 5) {
+      document.title= `New message ${value}`;
+  }
+}, [value]); // array is a list of dependencies
+// only invokes when it renders if it has a second parameter in useEffect
+// when second parameter has some value in array, whenever something happens for a second parameter
+// useEffect invokes
+
+  useEffect(() => {
+    console.log('hello world')
+  }, []) 
+
+  console.log('render component')
+  return <>
+    <h1>{value}</h1>
+    <button className="btn" onClick={() => {setValue(value + 1)} }>
+      Click me
+    </button>
+  </>;
 };
 
 export default UseEffectBasics;
